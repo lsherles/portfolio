680f39bb482da268b4af0baf323be19e891c8514
Lucas Sherles <lucasherles@gmail.com>
2025-04-15 16:21:25 -0700

    changes

---
+++ global.js
@@ -0,0 +1,97 @@
+console.log('IT’S ALIVE!');
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+// const navLinks = $$("nav a");
+
+// #const currentLink = navLinks.find(
+//   (a) => a.host === location.host && a.pathname === location.pathname
+// );
+
+// currentLink?.classList.add('current');
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'resume/', title: 'Resume' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'https://github.com/lsherles', title: 'GitHub' },
+  ];
+
+
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+? "/"                  // Local server
+: "/https://github.com/lsherles/portfolio";         // GitHub Pages repo name
+
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    url = !url.startsWith('http') ? BASE_PATH + url : url;
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
+    a.classList.toggle(
+        'current',
+        a.host === location.host && a.pathname === location.pathname,
+      );
+    if (a.host !== location.host) {
+      a.target = "_blank";
+    }
+  }
+
+  document.body.insertAdjacentHTML(
+    'afterbegin',
+    `
+    <label class="color-scheme">
+      Theme:
+      <select>
+        <option value="light dark">Automatic</option>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+      </select>
+    </label>`
+  ); 
+  
+let select = document.querySelector('.color-scheme select');
+
+select.addEventListener('input', function (event) {
+  let value = event.target.value;
+  document.documentElement.style.setProperty('color-scheme', value);
+
+  localStorage.colorScheme = value;
+});
+
+if ("colorScheme" in localStorage) {
+    let saved = localStorage.colorScheme;
+    document.documentElement.style.setProperty('color-scheme', saved);
+    select.value = saved;
+  }
+
+  let form = document.querySelector('form');
+
+  form?.addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent the default form submission
+  
+    let data = new FormData(form);
+    let params = [];
+  
+    for (let [name, value] of data) {
+      params.push(`${name}=${encodeURIComponent(value)}`);
+    }
+  
+    let url = `${form.action}?${params.join('&')}`;
+    location.href = url; // This opens the mail client
+  });
+  
+  
+
+
+
